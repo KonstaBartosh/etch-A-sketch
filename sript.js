c7a29538d4e37b38aba9e0aad81d6755a4ab5f62
Konstantin Bartosh <rustigillo@gmail.com>
2023-03-05 20:02:05 +0600

    Feat: dinamic change grid size via js

--- sript.js
+++ sript.js
@@ -1,19 +1,19 @@
 const template = document.querySelector('.template').content; // finding template in html
 const gridContainer = document.querySelector('.grid'); // finding empty div container in html
-let gridSize = 16;
+let gridSize = 10;
 
 function generateCells () {
-    for (let i = 0; i < gridSize ** 2; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
         const cell = template.querySelector('.cell').cloneNode(true); // deep cloning element
+        gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
         gridContainer.prepend(cell);
         
         cell.addEventListener('mouseover', function (evt) {
-            if (evt.buttons === 1) {
-                console.log(evt.buttons)
+            if (evt.buttons === 1) { // проверяем, что зажата левая кнопка мыши
                 cell.classList.add('cell_active')
             }
         });
     }
 }
-
 generateCells();
